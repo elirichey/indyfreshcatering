82895b81021a720b7b26d6baaddfb2a460039950
Eli Richey <eli@elirichey.com>
2017-06-19 20:00:02 -0400

    added testimonial ticker to home, new content & meta

--- _site/js/mobile-menu.js
+++ _site/js/mobile-menu.js
@@ -5,7 +5,7 @@
 $(document).ready(function() {
     $('.mobile-menu-base').hide().before('<a href="#" id="toggle-mobile-menu-base" class="button">MAIN MENU</a>');
     $('a#toggle-mobile-menu-base').click(function() {
-        $('.mobile-menu-base').slideToggle(1000);
+        $('.mobile-menu-base').slideToggle(250);
         return false;
     });
 });
@@ -18,7 +18,7 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.mobile-menu-menu').hide().before('<a href="#" id="toggle-mobile-menu-menu" class="button">MENU</a>');
     $('a#toggle-mobile-menu-menu').click(function() {
-        $('.mobile-menu-menu').slideToggle(1000);
+        $('.mobile-menu-menu').slideToggle(250);
         return false;
     });
 });
@@ -26,7 +26,7 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.mobile-menu-packages').hide().before('<a href="#" id="toggle-mobile-menu-packages" class="button">PACKAGES</a>');
     $('a#toggle-mobile-menu-packages').click(function() {
-        $('.mobile-menu-packages').slideToggle(1000);
+        $('.mobile-menu-packages').slideToggle(250);
         return false;
     });
 });
@@ -34,7 +34,7 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.mobile-menu-events').hide().before('<a href="#" id="toggle-mobile-menu-events" class="button">EVENTS</a>');
     $('a#toggle-mobile-menu-events').click(function() {
-        $('.mobile-menu-events').slideToggle(1000);
+        $('.mobile-menu-events').slideToggle(250);
         return false;
     });
 });
